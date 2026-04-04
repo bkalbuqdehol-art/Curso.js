d087e2d065577142ac99bd6246865f6e3b897d0b
bkalbuqdehol-art <bkalbuqdehol@gmail.com>
2026-04-04 20:17:08 -0300

    Implement log function for custom console outputy
    Added a log function to display messages in a custom console.

--- aula.js
+++ aula.js
@@ -1,1 +1,8 @@
-alert("Pedro é gay");
+function log(mensagem) {
+    const consoleTela = document.getElementById('console-falso');
+    consoleTela.innerHTML += "> " + mensagem + "<br>";
+}
+
+// AGORA USE ASSIM:
+print("Olá! Este é o meu console no Xbox.");
+print(10 + 20);
