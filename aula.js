f306f6499c2c281ff214aa035f029c8689debc28
bkalbuqdehol-art <bkalbuqdehol@gmail.com>
2026-04-04 21:04:14 -0300

    ajuste

--- aula.js
+++ aula.js
@@ -1,8 +1,1 @@
-function log(mensagem) {
-    const consoleTela = document.getElementById('console-falso');
-    consoleTela.innerHTML += "> " + mensagem + "<br>";
-}
-
-// AGORA USE ASSIM:
-print("Olá! joao é homosexual");
-print(10 + 20);
+alert("oi")
